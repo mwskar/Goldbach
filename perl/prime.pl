c86b9313aa94af23917fed0b082479a8bbb97d47
Michael W. Skarbek <skarbek_mw@cobra.cs.mercer.edu>
2021-09-07 15:22:11 -0400

    Fixed the algorithm to skip even numbers and check for sqares of odds

--- perl/prime.pl
+++ perl/prime.pl
@@ -3,13 +3,9 @@
 
 sub isPrime
 {
-	if (@_[0] == 1 || @_[0] == 3 || @_[0] == 5)
+	if (@_[0] == 1 || @_[0] == 3 || @_[0] == 5 || @_[0] == 7)
 	{
 		return 1;
-	}
-	elsif (@_[0]% 2 == 0)
-	{
-		return 0;
 	}
 	elsif(@_[0] % 3 == 0)
 	{
@@ -19,20 +15,31 @@ sub isPrime
 	{
 		return 0;
 	}
-	else
+	elsif(@_[0] % 7 == 0)
 	{
-		return 1;
+		return 0;
+	} 
+
+	for($l = 11; $l * $l <= @_[0]; $l += 2)
+	{
+		if(@_[0] % $l == 0)
+		{
+			return 0;
+		}
 	}
 
+	return 1;
 }
 
-print "Please input an upper pound for primes: ";
+print "Please input an upper bound for primes: ";
 my $input = <>;
 
-for ($i = 1; $i < $input; $i++)
+for ($i = 1; $i<= $input; $i += 2)
 {
 	if (isPrime($i))
 	{
 		print "\n $i";
 	}
 }
+
+print "\n";
