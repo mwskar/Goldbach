05113a7181afef0c5f38534b998c18c9938af4d5
Michael W. Skarbek <skarbek_mw@cobra.cs.mercer.edu>
2021-09-07 20:59:15 -0400

    Added comments

--- perl/prime.pl
+++ perl/prime.pl
@@ -1,6 +1,13 @@
 #!/usr/bin/perl
 
 
+#A short program to list out all prime numbers between 1
+#and a user input
+
+
+
+#Determines if a number is prime by checking possibile dividers
+#It is assumed than an even number (not prime) is not input
 sub isPrime
 {
 	if (@_[0] == 1 || @_[0] == 3 || @_[0] == 5 || @_[0] == 7)
@@ -20,6 +27,9 @@ sub isPrime
 		return 0;
 	} 
 
+	#Checks odd numbers above 7,
+	#If a numer's square is out of bounds, then that number will not be pared with
+	#a number greater than itself
 	for($l = 11; $l * $l <= @_[0]; $l += 2)
 	{
 		if(@_[0] % $l == 0)
@@ -34,6 +44,8 @@ sub isPrime
 print "Please input an upper bound for primes: ";
 my $input = <>;
 
+
+#Iterates only odd numbers to save run-time
 for ($i = 1; $i<= $input; $i += 2)
 {
 	if (isPrime($i))
