94e1fcab87db23faa816e799c4b3c3342543af7d
Michael W. Skarbek <skarbek_mw@cobra.cs.mercer.edu>
2021-09-14 12:07:54 -0400

    Now has a subroutine and a function, returns only odds now

--- fortran/prime.f95
+++ fortran/prime.f95
@@ -3,7 +3,33 @@ program prime
 integer i
 
 do i = 0, 100,1
-        print*, i
+        call printPrime(i)
 end do
+
 end program
+
+
+subroutine printPrime(i)
+        integer i
+        logical isPrime
+        if (isPrime(i)) then
+                print *, i
+        end if
+end subroutine printPrime
+
+
+
+logical function isPrime(i)
+
+implicit none
+
+integer i
+
+if (mod(i,2) /= 0) then
+        isPrime = .true.
+else 
+        isPrime = .false.
+end if
+
+end function isPrime
 
